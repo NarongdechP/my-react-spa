083ae69a5cf28773855c24dc355c1247c84623a8
NarongdechP <narongdech.pho@dome.tu.ac.th>
2025-01-28 12:11:19 +0700

    Add code

--- my-react-spa/src/App.jsx
+++ my-react-spa/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
   const [count, setCount] = useState(0)
